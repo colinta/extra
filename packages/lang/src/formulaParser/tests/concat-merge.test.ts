35a6e10f339e0eb43804de6ed687f3377021f76e
Colin T.A. Gray <colinta@gmail.com>
2025-05-12 07:22:01 -0400

    more concat-array tests

--- packages/lang/src/formulaParser/tests/concat-merge.test.ts
+++ packages/lang/src/formulaParser/tests/concat-merge.test.ts
@@ -20,10 +20,14 @@ describe('getType', () => {
   describe('string concatenation operator', () => {
     beforeEach(() => {
       runtimeTypes['str'] = [Types.string(), Values.string('test')]
+      runtimeTypes['str4'] = [Types.string({min: 4}), Values.string('test')]
     })
     cases<[string, Types.Type]>(
       c(['"test" <> "test"', Types.literal('testtest')]),
       c(['str <> str', Types.string()]),
+      c(['str <> str4', Types.string({min: 4})]),
+      c(['str <> "test"', Types.string({min: 4})]),
+      c(['str4 <> "test"', Types.string({min: 8})]),
     ).run(([formula, expected], {only, skip}) =>
       (only ? it.only : skip ? it.skip : it)(`${formula} => ${expected}`, () => {
         const expression = parse(formula).get()
@@ -34,24 +38,54 @@ describe('getType', () => {
 
   describe('array concatenation operator', () => {
     beforeEach(() => {
-      runtimeTypes['testString1'] = [Types.string(), Values.string('testString1')]
-      runtimeTypes['testString2'] = [Types.string(), Values.string('testString2')]
+      runtimeTypes['testString'] = [Types.string(), Values.string('testString')]
+      runtimeTypes['testString2'] = [Types.string({min: 2}), Values.string('testString2')]
       runtimeTypes['testInt'] = [Types.int(), Values.int(0)]
     })
     cases<[string, Types.Type]>(
       c(['["test"] ++ ["test"]', Types.literal('test')]),
       c(['["test1"] ++ ["test2"]', Types.oneOf([Types.literal('test1'), Types.literal('test2')])]),
-      c(['[testString1] ++ [testString2]', Types.string()]),
-      c(['[testString1] ++ [testInt]', Types.oneOf([Types.string(), Types.int()])]),
+      c(['[testString] ++ [testString2]', Types.string()]),
+      c(['[testString2] ++ [testString2]', Types.string({min: 2})]),
+      c(['[testString] ++ [testInt]', Types.oneOf([Types.string(), Types.int()])]),
       c(['[testInt] ++ [testInt]', Types.int()]),
       c(['[1] ++ [1.0]', Types.oneOf([Types.literal(1), Types.literal(1, 'float')])]),
       c(['[1.1] ++ [1.1]', Types.literal(1.1)]),
     ).run(([formula, expected], {only, skip}) =>
-      (only ? it.only : skip ? it.skip : it)(`${formula} => ${expected}`, () => {
+      (only ? it.only : skip ? it.skip : it)(`${formula} => Array(${expected}, length: =2)`, () => {
         const expression = parse(formula).get()
         expect(expression.getType(typeRuntime).get()).toEqual(
           Types.array(expected, {min: 2, max: 2}),
         )
+      }),
+    )
+
+    beforeEach(() => {
+      runtimeTypes['ary'] = [Types.array(Types.int()), Values.int(0)]
+      runtimeTypes['aryBigInt'] = [Types.array(Types.int({min: 10})), Values.int(0)]
+      runtimeTypes['ary3'] = [Types.array(Types.int(), {min: 3}), Values.int(0)]
+      runtimeTypes['aryMax3'] = [Types.array(Types.int(), {max: 3}), Values.int(0)]
+    })
+    cases<[string, Types.Type]>(
+      c(['ary ++ [1]', Types.array(Types.int(), {min: 1})]),
+      c(['ary ++ [1.1]', Types.array(Types.float(), {min: 1})]),
+      c(['ary ++ ary', Types.array(Types.int())]),
+      c(['ary ++ ary3', Types.array(Types.int(), {min: 3})]),
+      c(['ary3 ++ [1]', Types.array(Types.int(), {min: 4})]),
+      c(['aryMax3 ++ [1]', Types.array(Types.int(), {min: 1, max: 4})]),
+      c(['ary ++ aryMax3', Types.array(Types.int())]),
+      c(['ary3 ++ ary3', Types.array(Types.int(), {min: 6})]),
+      c(['ary3 ++ aryMax3', Types.array(Types.int(), {min: 3})]),
+      c(['aryMax3 ++ aryMax3', Types.array(Types.int(), {max: 6})]),
+      c([
+        'aryBigInt ++ [1]',
+        Types.array(Types.oneOf([Types.int({min: 10}), Types.literal(1)]), {min: 1}),
+      ]),
+      c(['aryBigInt ++ [11]', Types.array(Types.int({min: 10}), {min: 1})]),
+    ).run(([formula, expected], {only, skip}) =>
+      (only ? it.only : skip ? it.skip : it)(`${formula} => ${expected}`, () => {
+        const expression = parse(formula).get()
+        expect(expression.getType(typeRuntime).get()).toEqual(expected)
       }),
     )
   })
