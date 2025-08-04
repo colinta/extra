2645386f70ff17d2a552308689017c8e244144c0
Colin T.A. Gray <colinta@gmail.com>
2025-08-03 22:04:28 -0400

    test combineOr

---
+++ packages/lang/src/formulaParser/tests/combineOrRelationships.test.ts
@@ -0,0 +1,72 @@
+import {c, cases} from '@extra-lang/cases'
+import {type TypeRuntime, type ValueRuntime} from '../../runtime'
+import * as Types from '../../types'
+import * as Values from '../../values'
+import {parse} from '../../formulaParser'
+import {type Expression} from '../expressions'
+import {mockTypeRuntime} from '../../tests/mockTypeRuntime'
+import {mockValueRuntime} from '../../tests/mockValueRuntime'
+
+let runtimeTypes: {[K in string]: [Types.Type, Values.Value]}
+
+let typeRuntime: TypeRuntime
+let valueRuntime: ValueRuntime
+
+beforeEach(() => {
+  runtimeTypes = {}
+  typeRuntime = mockTypeRuntime(runtimeTypes)
+  valueRuntime = mockValueRuntime(runtimeTypes)
+  ;(_ => {})(valueRuntime)
+})
+
+/**
+ * Used in the match eval tests
+ */
+function truthyFalsey(name: string, expression: Expression, runtime: TypeRuntime) {
+  const truthy = expression.assumeTrue(runtime).get()
+  const falsey = expression.assumeFalse(runtime).get()
+  return {truthy: truthy.getLocalType(name), falsey: falsey.getLocalType(name)}
+}
+
+describe('combineOrRelationships', () => {
+  describe('getType', () => {
+    cases<
+      [
+        Types.Type,
+        string,
+        {
+          truthy: Types.Type
+          falsey: Types.Type
+        },
+      ]
+    >(
+      c([
+        Types.oneOf([Types.string(), Types.int(), Types.booleanType()]),
+        'foo is String or foo is Int',
+        {
+          truthy: Types.oneOf([Types.string(), Types.int()]),
+          falsey: Types.booleanType(),
+        },
+      ]),
+      c([
+        Types.oneOf([Types.string(), Types.int(), Types.booleanType(), Types.nullType()]),
+        'foo is String or foo is Int or foo is Boolean',
+        {
+          truthy: Types.oneOf([Types.string(), Types.int(), Types.booleanType()]),
+          falsey: Types.nullType(),
+        },
+      ]),
+    ).run(([fooType, formula, expectedTypes], {only, skip}) => {
+      ;(only ? it.only : skip ? it.skip : it)(
+        `(foo: ${fooType}) '${formula}' => truthy: ${expectedTypes.truthy}, falsey: ${expectedTypes.falsey}`,
+        () => {
+          runtimeTypes['foo'] = [fooType, Values.nullValue()]
+          const expression = parse(formula).get()
+          const {truthy, falsey} = truthyFalsey('foo', expression, typeRuntime)
+          expect(truthy).toEqual(expectedTypes.truthy)
+          expect(falsey).toEqual(expectedTypes.falsey)
+        },
+      )
+    })
+  })
+})
