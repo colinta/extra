2ebff09b7aafdaf59628a735d27fb36eaffafe9e
Colin T.A. Gray <colinta@gmail.com>
2025-08-12 07:14:58 -0400

    first jsx test

--- packages/lang/src/formulaParser/tests/jsx.test.ts
+++ packages/lang/src/formulaParser/tests/jsx.test.ts
@@ -1,5 +1,6 @@
 import {c, cases} from '@extra-lang/cases'
-import {parse} from '../../formulaParser'
+import {parse, testScan} from '../../formulaParser'
+import {scanJsx} from '../scan/jsx'
 
 describe('jsx', () => {
   describe('parse', () => {
@@ -7,14 +8,14 @@ describe('jsx', () => {
       //
       c([
         //
-        '',
-        ``,
+        '<div />',
+        `<div />`,
       ]),
     ).run(([formula, expectedCode], {only, skip}) =>
       (only ? it.only : skip ? it.skip : it)(
         `'${formula}' should parse as '${expectedCode}'`,
         () => {
-          const expression = parse(formula).get()
+          const expression = testScan(formula, scanJsx)
           expect(expression.toCode()).toEqual(expectedCode)
         },
       ),
