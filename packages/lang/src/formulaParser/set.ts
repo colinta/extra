22090894382620d91179f8ff8b6ed2e32405af7c
Colin T.A. Gray <colinta@gmail.com>
2025-08-20 12:56:18 -0400

    set operations support multiple args

--- packages/lang/src/formulaParser/set.ts
+++ packages/lang/src/formulaParser/set.ts
@@ -1,14 +1,14 @@
 /**
  * Removes the items in setB (second arg) from setA (first arg).
  */
-export function difference<T>(setA: Set<T>, setB: Set<T>): Set<T> {
+export function difference<T>(setA: Set<T>, ...sets: Set<T>[]): Set<T> {
   if ((setA as any).difference) {
-    return (setA as any).difference(setB) as Set<T>
+    return sets.reduce((setA, setB) => (setA as any).difference(setB) as Set<T>, setA)
   }
 
   const _difference = new Set<T>()
   for (const elem of setA) {
-    if (setB.has(elem)) {
+    if (sets.some(setB => setB.has(elem))) {
       continue
     }
 
@@ -21,14 +21,16 @@ export function difference<T>(setA: Set<T>, setB: Set<T>): Set<T> {
 /**
  * Returns the intersection of two sets - the elements that are in either set.
  */
-export function union<T>(setA: Set<T>, setB: Set<T>): Set<T> {
+export function union<T>(setA: Set<T>, ...sets: Set<T>[]): Set<T> {
   if ((setA as any).union) {
-    return (setA as any).union(setB) as Set<T>
+    return sets.reduce((setA, setB) => (setA as any).union(setB) as Set<T>, setA)
   }
 
   const _union = new Set<T>(setA)
-  for (const elem of setB) {
-    _union.add(elem)
+  for (const setB of sets) {
+    for (const elem of setB) {
+      _union.add(elem)
+    }
   }
 
   return _union
@@ -37,14 +39,14 @@ export function union<T>(setA: Set<T>, setB: Set<T>): Set<T> {
 /**
  * Returns the intersection of two sets - the elements that are in both sets.
  */
-export function intersection<T>(setA: Set<T>, setB: Set<T>): Set<T> {
+export function intersection<T>(setA: Set<T>, ...sets: Set<T>[]): Set<T> {
   if ((setA as any).intersection) {
-    return (setA as any).intersection(setB) as Set<T>
+    return sets.reduce((setA, setB) => (setA as any).intersection(setB) as Set<T>, setA)
   }
 
   const _intersection = new Set<T>()
   for (const elem of setA) {
-    if (setB.has(elem)) {
+    if (sets.some(setB => setB.has(elem))) {
       _intersection.add(elem)
     }
   }
